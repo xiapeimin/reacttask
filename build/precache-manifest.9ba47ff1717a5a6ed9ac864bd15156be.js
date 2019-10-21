7e2743e796c600ded53ab4381b55209ebcfd6bb3
xiapeimin <xpm2536733090>
2019-10-21 20:55:17 +0800

    task

---
+++ build/precache-manifest.9ba47ff1717a5a6ed9ac864bd15156be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d811c0908166f9d5e4788a2a2e849da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3589ce27cd770277e21a",
+    "url": "/static/css/2.802837ff.chunk.css"
+  },
+  {
+    "revision": "5afbe6c04e9760473ff2",
+    "url": "/static/css/main.f7a915e7.chunk.css"
+  },
+  {
+    "revision": "3589ce27cd770277e21a",
+    "url": "/static/js/2.321e8bde.chunk.js"
+  },
+  {
+    "revision": "5afbe6c04e9760473ff2",
+    "url": "/static/js/main.9aa4b966.chunk.js"
+  },
+  {
+    "revision": "83d97cf838c74a71b465",
+    "url": "/static/js/runtime-main.ab3ec558.js"
+  }
+]);
